87e1d7fd965cc3ac65b333651daf4ff72a29ef66
Staś Małolepszy <stas@piesku.com>
2021-04-14 13:50:48 +0200

    Bootstrap src from PathFinding

--- Animate/components/com_control.ts
+++
@@ -1,8 +0,0 @@
-import {Entity, Game} from "../game.js";
-import {Has} from "../world.js";
-
-export function control() {
-    return (game: Game, entity: Entity) => {
-        game.World.Signature[entity] |= Has.Control;
-    };
-}
