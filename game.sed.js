b1dd5df392924c7cfa762ac95901dd16b3d05b2f
stasm <stas@duzodobrze.pl>
2021-04-24 22:18:13 +0000

    Deploying to gh-pages from @ michalbe/risk@32a43a170a9da290d8e719b8a3bb4c2be6237342 🚀

--- game.sed.js
+++ game.sed.js
@@ -40164,6 +40164,296 @@ throw new Error(`Failed to set up the framebuffer (${status}).`);
 return target;
 }
 
+let seed = 1;
+function set_seed(new_seed) {
+seed = 198706 * new_seed;
+}
+function rand() {
+seed = (seed * 16807) % 2147483647;
+return (seed - 1) / 2147483646;
+}
+function integer(min = 0, max = 1) {
+return ~~(rand() * (max - min + 1) + min);
+}
+function float(min = 0, max = 1) {
+return rand() * (max - min) + min;
+}
+function element(arr) {
+return arr[integer(0, arr.length - 1)];
+}
+
+function create() {
+return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+}
+function copy$2(out, a) {
+out[0] = a[0];
+out[1] = a[1];
+out[2] = a[2];
+out[3] = a[3];
+out[4] = a[4];
+out[5] = a[5];
+out[6] = a[6];
+out[7] = a[7];
+out[8] = a[8];
+out[9] = a[9];
+out[10] = a[10];
+out[11] = a[11];
+out[12] = a[12];
+out[13] = a[13];
+out[14] = a[14];
+out[15] = a[15];
+return out;
+}
+function invert(out, a) {
+let a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3];
+let a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
+let a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
+let a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
+let b00 = a00 * a11 - a01 * a10;
+let b01 = a00 * a12 - a02 * a10;
+let b02 = a00 * a13 - a03 * a10;
+let b03 = a01 * a12 - a02 * a11;
+let b04 = a01 * a13 - a03 * a11;
+let b05 = a02 * a13 - a03 * a12;
+let b06 = a20 * a31 - a21 * a30;
+let b07 = a20 * a32 - a22 * a30;
+let b08 = a20 * a33 - a23 * a30;
+let b09 = a21 * a32 - a22 * a31;
+let b10 = a21 * a33 - a23 * a31;
+let b11 = a22 * a33 - a23 * a32;
+let det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+if (!det) {
+return null;
+}
+det = 1.0 / det;
+out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+return out;
+}
+function multiply$1(out, a, b) {
+let a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3];
+let a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
+let a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
+let a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15]; 
+let b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
+out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+b0 = b[4];
+b1 = b[5];
+b2 = b[6];
+b3 = b[7];
+out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+b0 = b[8];
+b1 = b[9];
+b2 = b[10];
+b3 = b[11];
+out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+b0 = b[12];
+b1 = b[13];
+b2 = b[14];
+b3 = b[15];
+out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+return out;
+}
+function from_rotation_translation_scale(out, q, v, s) {
+
+let x = q[0], y = q[1], z = q[2], w = q[3];
+let x2 = x + x;
+let y2 = y + y;
+let z2 = z + z;
+let xx = x * x2;
+let xy = x * y2;
+let xz = x * z2;
+let yy = y * y2;
+let yz = y * z2;
+let zz = z * z2;
+let wx = w * x2;
+let wy = w * y2;
+let wz = w * z2;
+let sx = s[0];
+let sy = s[1];
+let sz = s[2];
+out[0] = (1 - (yy + zz)) * sx;
+out[1] = (xy + wz) * sx;
+out[2] = (xz - wy) * sx;
+out[3] = 0;
+out[4] = (xy - wz) * sy;
+out[5] = (1 - (xx + zz)) * sy;
+out[6] = (yz + wx) * sy;
+out[7] = 0;
+out[8] = (xz + wy) * sz;
+out[9] = (yz - wx) * sz;
+out[10] = (1 - (xx + yy)) * sz;
+out[11] = 0;
+out[12] = v[0];
+out[13] = v[1];
+out[14] = v[2];
+out[15] = 1;
+return out;
+}
+function perspective(out, fovy, aspect, near, far) {
+let f = 1.0 / Math.tan(fovy / 2), nf;
+out[0] = f / aspect;
+out[1] = 0;
+out[2] = 0;
+out[3] = 0;
+out[4] = 0;
+out[5] = f;
+out[6] = 0;
+out[7] = 0;
+out[8] = 0;
+out[9] = 0;
+out[11] = -1;
+out[12] = 0;
+out[13] = 0;
+out[15] = 0;
+if (far != null && far !== Infinity) {
+nf = 1 / (near - far);
+out[10] = (far + near) * nf;
+out[14] = 2 * far * near * nf;
+}
+else {
+out[10] = -1;
+out[14] = -2 * near;
+}
+return out;
+}
+function ortho(out, top, right, bottom, left, near, far) {
+let lr = 1 / (left - right);
+let bt = 1 / (bottom - top);
+let nf = 1 / (near - far);
+out[0] = -2 * lr;
+out[1] = 0;
+out[2] = 0;
+out[3] = 0;
+out[4] = 0;
+out[5] = -2 * bt;
+out[6] = 0;
+out[7] = 0;
+out[8] = 0;
+out[9] = 0;
+out[10] = 2 * nf;
+out[11] = 0;
+out[12] = (left + right) * lr;
+out[13] = (top + bottom) * bt;
+out[14] = (far + near) * nf;
+out[15] = 1;
+return out;
+}
+function get_translation(out, mat) {
+out[0] = mat[12];
+out[1] = mat[13];
+out[2] = mat[14];
+return out;
+}
+
+function copy$1(out, a) {
+out[0] = a[0];
+out[1] = a[1];
+out[2] = a[2];
+return out;
+}
+function add(out, a, b) {
+out[0] = a[0] + b[0];
+out[1] = a[1] + b[1];
+out[2] = a[2] + b[2];
+return out;
+}
+function subtract(out, a, b) {
+out[0] = a[0] - b[0];
+out[1] = a[1] - b[1];
+out[2] = a[2] - b[2];
+return out;
+}
+function scale(out, a, b) {
+out[0] = a[0] * b;
+out[1] = a[1] * b;
+out[2] = a[2] * b;
+return out;
+}
+function negate(out, a) {
+out[0] = -a[0];
+out[1] = -a[1];
+out[2] = -a[2];
+return out;
+}
+function normalize(out, a) {
+let x = a[0];
+let y = a[1];
+let z = a[2];
+let len = x * x + y * y + z * z;
+if (len > 0) {
+//TODO: evaluate use of glm_invsqrt here?
+len = 1 / Math.sqrt(len);
+}
+out[0] = a[0] * len;
+out[1] = a[1] * len;
+out[2] = a[2] * len;
+return out;
+}
+function dot(a, b) {
+return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+function cross(out, a, b) {
+let ax = a[0], ay = a[1], az = a[2];
+let bx = b[0], by = b[1], bz = b[2];
+out[0] = ay * bz - az * by;
+out[1] = az * bx - ax * bz;
+out[2] = ax * by - ay * bx;
+return out;
+}
+function transform_point(out, a, m) {
+let x = a[0], y = a[1], z = a[2];
+let w = m[3] * x + m[7] * y + m[11] * z + m[15];
+w = w || 1.0;
+out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
+out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
+out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
+return out;
+}
+function transform_direction(out, a, m) {
+let tip = transform_point([0, 0, 0], a, m);
+let base = get_translation([0, 0, 0], m);
+return subtract(out, tip, base);
+}
+function length(a) {
+let x = a[0];
+let y = a[1];
+let z = a[2];
+return Math.hypot(x, y, z);
+}
+function distance_squared(a, b) {
+let x = b[0] - a[0];
+let y = b[1] - a[1];
+let z = b[2] - a[2];
+return x * x + y * y + z * z;
+}
+
 function link(gl, vertex, fragment) {
 let program = gl.createProgram();
 gl.attachShader(program, compile(gl, GL_VERTEX_SHADER, vertex));
@@ -40182,6 +40472,72 @@ if (!gl.getShaderParameter(shader, GL_COMPILE_STATUS)) {
 throw new Error(gl.getShaderInfoLog(shader));
 }
 return shader;
+}
+const up = [0, 1, 0];
+function random_point_up_worldspace(mesh, world_space) {
+let point_localspace = random_point_up(mesh);
+if (point_localspace === null) {
+return null;
+}
+return transform_point(point_localspace, point_localspace, world_space);
+}
+function random_point_up(mesh) {
+let up_face_indices = [];
+let face_count = mesh.IndexCount / 3;
+for (let f = 0; f < face_count; f++) {
+let v0 = mesh.IndexArray[f * 3 + 0];
+let v1 = mesh.IndexArray[f * 3 + 1];
+let v2 = mesh.IndexArray[f * 3 + 2];
+let n = normal(mesh.VertexArray, v0, v1, v2);
+if (dot(n, up) === 1) {
+up_face_indices.push(f);
+}
+}
+if (up_face_indices.length === 0) {
+
+return null;
+}
+let f = element(up_face_indices);
+let v0 = mesh.IndexArray[f * 3 + 0];
+let v1 = mesh.IndexArray[f * 3 + 1];
+let v2 = mesh.IndexArray[f * 3 + 2];
+let p0 = [
+mesh.VertexArray[v0 * 3 + 0],
+mesh.VertexArray[v0 * 3 + 1],
+mesh.VertexArray[v0 * 3 + 2],
+];
+let p1 = [
+mesh.VertexArray[v1 * 3 + 0],
+mesh.VertexArray[v1 * 3 + 1],
+mesh.VertexArray[v1 * 3 + 2],
+];
+let p2 = [
+mesh.VertexArray[v2 * 3 + 0],
+mesh.VertexArray[v2 * 3 + 1],
+mesh.VertexArray[v2 * 3 + 2],
+];
+
+let t0 = float(0, 1);
+let t1 = float(0, 1);
+if (t0 + t1 > 1) {
+t0 = 1 - t0;
+t1 = 1 - t1;
+}
+let t2 = 1 - t0 - t1;
+
+return [
+t0 * p0[0] + t1 * p1[0] + t2 * p2[0],
+t0 * p0[1] + t1 * p1[1] + t2 * p2[1],
+t0 * p0[2] + t1 * p1[2] + t2 * p2[2],
+];
+}
+function normal(vertices, a, b, c) {
+let edge1 = [0, 0, 0];
+let edge2 = [0, 0, 0];
+subtract(edge1, [vertices[b * 3 + 0], vertices[b * 3 + 1], vertices[b * 3 + 2]], [vertices[a * 3 + 0], vertices[a * 3 + 1], vertices[a * 3 + 2]]);
+subtract(edge2, [vertices[c * 3 + 0], vertices[c * 3 + 1], vertices[c * 3 + 2]], [vertices[b * 3 + 0], vertices[b * 3 + 1], vertices[b * 3 + 2]]);
+let product = cross([0, 0, 0], edge2, edge1);
+return normalize(product, product);
 }
 
 let vertex$3 = `
@@ -62051,278 +62407,6 @@ function loop_stop() {
 cancelAnimationFrame(raf);
 }
 
-function copy$2(out, a) {
-out[0] = a[0];
-out[1] = a[1];
-out[2] = a[2];
-return out;
-}
-function add(out, a, b) {
-out[0] = a[0] + b[0];
-out[1] = a[1] + b[1];
-out[2] = a[2] + b[2];
-return out;
-}
-function subtract(out, a, b) {
-out[0] = a[0] - b[0];
-out[1] = a[1] - b[1];
-out[2] = a[2] - b[2];
-return out;
-}
-function scale(out, a, b) {
-out[0] = a[0] * b;
-out[1] = a[1] * b;
-out[2] = a[2] * b;
-return out;
-}
-function negate(out, a) {
-out[0] = -a[0];
-out[1] = -a[1];
-out[2] = -a[2];
-return out;
-}
-function normalize(out, a) {
-let x = a[0];
-let y = a[1];
-let z = a[2];
-let len = x * x + y * y + z * z;
-if (len > 0) {
-//TODO: evaluate use of glm_invsqrt here?
-len = 1 / Math.sqrt(len);
-}
-out[0] = a[0] * len;
-out[1] = a[1] * len;
-out[2] = a[2] * len;
-return out;
-}
-function dot(a, b) {
-return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-function cross(out, a, b) {
-let ax = a[0], ay = a[1], az = a[2];
-let bx = b[0], by = b[1], bz = b[2];
-out[0] = ay * bz - az * by;
-out[1] = az * bx - ax * bz;
-out[2] = ax * by - ay * bx;
-return out;
-}
-function transform_point(out, a, m) {
-let x = a[0], y = a[1], z = a[2];
-let w = m[3] * x + m[7] * y + m[11] * z + m[15];
-w = w || 1.0;
-out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
-out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
-out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
-return out;
-}
-function transform_direction(out, a, m) {
-let tip = transform_point([0, 0, 0], a, m);
-let base = get_translation([0, 0, 0], m);
-return subtract(out, tip, base);
-}
-function length(a) {
-let x = a[0];
-let y = a[1];
-let z = a[2];
-return Math.hypot(x, y, z);
-}
-function distance_squared(a, b) {
-let x = b[0] - a[0];
-let y = b[1] - a[1];
-let z = b[2] - a[2];
-return x * x + y * y + z * z;
-}
-
-function create() {
-return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-}
-function copy$1(out, a) {
-out[0] = a[0];
-out[1] = a[1];
-out[2] = a[2];
-out[3] = a[3];
-out[4] = a[4];
-out[5] = a[5];
-out[6] = a[6];
-out[7] = a[7];
-out[8] = a[8];
-out[9] = a[9];
-out[10] = a[10];
-out[11] = a[11];
-out[12] = a[12];
-out[13] = a[13];
-out[14] = a[14];
-out[15] = a[15];
-return out;
-}
-function invert(out, a) {
-let a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3];
-let a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
-let a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
-let a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
-let b00 = a00 * a11 - a01 * a10;
-let b01 = a00 * a12 - a02 * a10;
-let b02 = a00 * a13 - a03 * a10;
-let b03 = a01 * a12 - a02 * a11;
-let b04 = a01 * a13 - a03 * a11;
-let b05 = a02 * a13 - a03 * a12;
-let b06 = a20 * a31 - a21 * a30;
-let b07 = a20 * a32 - a22 * a30;
-let b08 = a20 * a33 - a23 * a30;
-let b09 = a21 * a32 - a22 * a31;
-let b10 = a21 * a33 - a23 * a31;
-let b11 = a22 * a33 - a23 * a32;
-let det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-if (!det) {
-return null;
-}
-det = 1.0 / det;
-out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
-out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
-out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
-out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
-out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
-out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
-out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
-out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
-out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
-out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
-out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
-out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
-out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
-out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
-out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
-out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
-return out;
-}
-function multiply$1(out, a, b) {
-let a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3];
-let a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
-let a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
-let a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15]; 
-let b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
-out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-b0 = b[4];
-b1 = b[5];
-b2 = b[6];
-b3 = b[7];
-out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-b0 = b[8];
-b1 = b[9];
-b2 = b[10];
-b3 = b[11];
-out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-b0 = b[12];
-b1 = b[13];
-b2 = b[14];
-b3 = b[15];
-out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-return out;
-}
-function from_rotation_translation_scale(out, q, v, s) {
-
-let x = q[0], y = q[1], z = q[2], w = q[3];
-let x2 = x + x;
-let y2 = y + y;
-let z2 = z + z;
-let xx = x * x2;
-let xy = x * y2;
-let xz = x * z2;
-let yy = y * y2;
-let yz = y * z2;
-let zz = z * z2;
-let wx = w * x2;
-let wy = w * y2;
-let wz = w * z2;
-let sx = s[0];
-let sy = s[1];
-let sz = s[2];
-out[0] = (1 - (yy + zz)) * sx;
-out[1] = (xy + wz) * sx;
-out[2] = (xz - wy) * sx;
-out[3] = 0;
-out[4] = (xy - wz) * sy;
-out[5] = (1 - (xx + zz)) * sy;
-out[6] = (yz + wx) * sy;
-out[7] = 0;
-out[8] = (xz + wy) * sz;
-out[9] = (yz - wx) * sz;
-out[10] = (1 - (xx + yy)) * sz;
-out[11] = 0;
-out[12] = v[0];
-out[13] = v[1];
-out[14] = v[2];
-out[15] = 1;
-return out;
-}
-function perspective(out, fovy, aspect, near, far) {
-let f = 1.0 / Math.tan(fovy / 2), nf;
-out[0] = f / aspect;
-out[1] = 0;
-out[2] = 0;
-out[3] = 0;
-out[4] = 0;
-out[5] = f;
-out[6] = 0;
-out[7] = 0;
-out[8] = 0;
-out[9] = 0;
-out[11] = -1;
-out[12] = 0;
-out[13] = 0;
-out[15] = 0;
-if (far != null && far !== Infinity) {
-nf = 1 / (near - far);
-out[10] = (far + near) * nf;
-out[14] = 2 * far * near * nf;
-}
-else {
-out[10] = -1;
-out[14] = -2 * near;
-}
-return out;
-}
-function ortho(out, top, right, bottom, left, near, far) {
-let lr = 1 / (left - right);
-let bt = 1 / (bottom - top);
-let nf = 1 / (near - far);
-out[0] = -2 * lr;
-out[1] = 0;
-out[2] = 0;
-out[3] = 0;
-out[4] = 0;
-out[5] = -2 * bt;
-out[6] = 0;
-out[7] = 0;
-out[8] = 0;
-out[9] = 0;
-out[10] = 2 * nf;
-out[11] = 0;
-out[12] = (left + right) * lr;
-out[13] = (top + bottom) * bt;
-out[14] = (far + near) * nf;
-out[15] = 1;
-return out;
-}
-function get_translation(out, mat) {
-out[0] = mat[12];
-out[1] = mat[13];
-out[2] = mat[14];
-return out;
-}
-
 const QUERY$g = 16384 /* Transform */ | 1 /* Camera */;
 function sys_camera(game, delta) {
 if (game.ViewportWidth != window.innerWidth || game.ViewportHeight != window.innerHeight) {
@@ -62362,7 +62446,7 @@ invert(camera.Unprojection, camera.Projection);
 }
 let transform = game.World.Transform[entity];
 get_translation(camera.Position, transform.World);
-copy$1(camera.View, transform.Self);
+copy$2(camera.View, transform.Self);
 multiply$1(camera.Pv, camera.Projection, camera.View);
 }
 function update_framebuffer_ortho(game, entity, camera) {
@@ -62372,7 +62456,7 @@ invert(camera.Unprojection, camera.Projection);
 }
 let transform = game.World.Transform[entity];
 get_translation(camera.Position, transform.World);
-copy$1(camera.View, transform.Self);
+copy$2(camera.View, transform.Self);
 multiply$1(camera.Pv, camera.Projection, camera.View);
 }
 
@@ -62523,21 +62607,6 @@ Hit: negate([0, 0, 0], hit),
 }
 }
 
-let seed = 1;
-function set_seed(new_seed) {
-seed = 198706 * new_seed;
-}
-function rand() {
-seed = (seed * 16807) % 2147483647;
-return (seed - 1) / 2147483646;
-}
-function integer(min = 0, max = 1) {
-return ~~(rand() * (max - min + 1) + min);
-}
-function float(min = 0, max = 1) {
-return rand() * (max - min) + min;
-}
-
 const QUERY$e = 512 /* NavAgent */ | 32768 /* Team */;
 function sys_control_ai(game, delta) {
 for (let i = 0; i < game.World.Signature.length; i++) {
@@ -62556,12 +62625,16 @@ if (agent.Actions > 0) {
 
 let territory_entity = game.TerritoryEntities[integer(1, 7)];
 let territory = game.World.Territory[territory_entity];
+console.log(territory);
 if (agent.TerritoryId !== territory.Id) {
 
 agent.Actions -= 1;
 }
+let territory_mesh = game.TerritoryMeshes[territory.Continent][territory.Index - 1];
+let territory_transform = game.World.Transform[territory_entity];
+let destination_worldspace = random_point_up_worldspace(territory_mesh, territory_transform.World);
 agent.TerritoryId = territory.Id;
-agent.Destination = [float(-40, 7), 0, float(-70, -50)];
+agent.Destination = destination_worldspace;
 }
 }
 }
@@ -63006,7 +63079,7 @@ let agent = game.World.NavAgent[entity];
 if (agent.Destination) {
 let transform = game.World.Transform[entity];
 let destination = [0, 0, 0];
-copy$2(destination, agent.Destination);
+copy$1(destination, agent.Destination);
 let position = [0, 0, 0];
 get_translation(position, transform.World);
 let distance_to_destination = distance_squared(position, agent.Destination);
