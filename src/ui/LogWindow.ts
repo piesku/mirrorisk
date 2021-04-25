6e4e7ca938807941dec2283a7acfb91ef8f2cc85
Michał Budzyński <michal@virtualdesign.pl>
2021-04-25 21:19:42 +0200

    smaller log window

--- src/ui/LogWindow.ts
+++ src/ui/LogWindow.ts
@@ -4,7 +4,7 @@ import {Game} from "../game.js";
 export function LogWindow(game: Game) {
     return html`<div
         class="window"
-        style="width: 500px; max-height: 400px; margin: 10px; position: absolute; bottom:0; right: 0;"
+        style="width: 500px; max-height: 300px; margin: 10px; position: absolute; bottom:0; right: 0;"
     >
         <div class="title-bar">
             <div class="title-bar-text">Game log</div>
@@ -17,7 +17,7 @@ export function LogWindow(game: Game) {
         <div class="window-body">
             <pre
                 class="pre-log"
-                style="white-space: pre-wrap;word-wrap: break-word; overflow-y: scroll;max-height:300px;"
+                style="white-space: pre-wrap;word-wrap: break-word; overflow-y: scroll;height:150px;"
             >
 Piesku&#10094;R&#10095; Mirrorisk
 &#10094;C&#10095; Copyright Piesku Corp 2001-2021.
