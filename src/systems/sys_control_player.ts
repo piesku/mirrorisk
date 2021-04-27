8d68d62dac7ec04337a5c6abe76164e458dd2bc9
Staś Małolepszy <stas@piesku.com>
2021-04-27 22:18:25 +0200

    Make sure game.Picked is a territory when giving orders

--- src/systems/sys_control_player.ts
+++ src/systems/sys_control_player.ts
@@ -27,20 +27,23 @@ function update(game: Game, entity: Entity) {
     let audio_source = game.World.AudioSource[entity];
 
     if (
+        // If the user clicks…
         game.InputDelta["Mouse2"] === -1 &&
         game.InputState["Mouse2DownTraveled"] < 10 &&
+        // …over a territory…
         game.Picked &&
+        game.World.Signature[game.Picked.Entity] & Has.Territory &&
+        // …and the army can move.
         agent.Actions > 0
     ) {
         let territory_entity = game.Picked.Entity;
         let territory = game.World.Territory[territory_entity];
 
-        audio_source.Trigger = game.Sounds[element(sfx)];
-
         if (!game.TerritoryGraph[agent.TerritoryId].includes(territory.Id)) {
             // This aint adjacent territory
             return;
         }
+
         if (agent.TerritoryId !== territory.Id) {
             // Use the action up only when moving to another territory.
             agent.Actions -= 1;
@@ -60,5 +63,7 @@ function update(game: Game, entity: Entity) {
         }
         agent.TerritoryId = territory.Id;
         agent.Destination = game.Picked.Point;
+
+        audio_source.Trigger = game.Sounds[element(sfx)];
     }
 }
