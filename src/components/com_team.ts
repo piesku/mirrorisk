078e27dc6d7f094853e676ffd5a7d1ea62bfda2c
Staś Małolepszy <stas@piesku.com>
2021-05-07 04:12:05 +0200

    Document Team.Actions

--- src/components/com_team.ts
+++ src/components/com_team.ts
@@ -3,6 +3,8 @@ import {Has} from "../world.js";
 
 export interface Team {
     Id: number;
+    /** Number of this unit's actions left this turn. When negative, the unit
+     * hasn't moved this turn yet but cannot move due to other constraints. */
     Actions: number;
 }
 
