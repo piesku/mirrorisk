98d42897e84cfced6ec98dbb1e8eedf303f2e13e
Staś Małolepszy <stas@piesku.com>
2021-05-13 04:40:40 +0200

    Don't resolve tasks while iterating them

--- src/systems/sys_poll.ts
+++ src/systems/sys_poll.ts
@@ -1,53 +1,60 @@
 import {TaskKind} from "../components/com_task.js";
 import {Entity, Game} from "../game.js";
-import {Has} from "../world.js";
+import {Has, World} from "../world.js";
 
 const QUERY = Has.Task;
-const CLOSE_ENOUGH_SQUARED = 1;
 
 export function sys_poll(game: Game, delta: number) {
+    // Collect all ready tasks first to avoid completing them while we stil
+    // literate over ohter tasks. This guarantees that tasks blocked by other
+    // tasks will be completed during the next frame.
+    let tasks_to_complete: Array<Entity> = [];
+
     for (let i = 0; i < game.World.Signature.length; i++) {
         if ((game.World.Signature[i] & QUERY) === QUERY) {
-            update(game, i, delta);
+            if (has_blocking_dependencies(game.World, i)) {
+                continue;
+            }
+
+            let task = game.World.Task[i];
+            switch (task.Kind) {
+                case TaskKind.Until: {
+                    if (task.Predicate()) {
+                        tasks_to_complete.push(i);
+                    }
+                    break;
+                }
+                case TaskKind.Timeout: {
+                    task.Remaining -= delta;
+                    if (task.Remaining < 0) {
+                        tasks_to_complete.push(i);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    for (let entity of tasks_to_complete) {
+        let task = game.World.Task[entity];
+
+        game.World.Signature[entity] &= ~Has.Task;
+        if (task.OnDone) {
+            task.OnDone();
         }
     }
 }
 
-function update(game: Game, entity: Entity, delta: number) {
-    // Does this task have dependencies that block it?
-    if (game.World.Signature[entity] & Has.Children) {
-        let children = game.World.Children[entity];
+function has_blocking_dependencies(world: World, entity: Entity) {
+    if (world.Signature[entity] & Has.Children) {
+        let children = world.Children[entity];
         for (let child of children.Children) {
-            if (game.World.Signature[child] & Has.Task) {
-                // A pending child means that the task cannot proceed right now.
-                return;
+            if (world.Signature[child] & Has.Task) {
+                // A pending child blocks the parent task.
+                return true;
             }
         }
     }
 
-    let task = game.World.Task[entity];
-    switch (task.Kind) {
-        case TaskKind.Until: {
-            if (task.Predicate()) {
-                // The task has completed.
-                game.World.Signature[entity] &= ~Has.Task;
-                if (task.OnDone) {
-                    task.OnDone();
-                }
-            }
-            break;
-        }
-        case TaskKind.Timeout: {
-            task.Remaining -= delta;
-            if (task.Remaining < 0) {
-                // The task has completed.
-                game.World.Signature[entity] &= ~Has.Task;
-                if (task.OnDone) {
-                    task.OnDone();
-                }
-            }
-
-            break;
-        }
-    }
+    return false;
 }
